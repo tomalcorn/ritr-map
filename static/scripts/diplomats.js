54ec7d13713817dd02d70faf6aaf0f86af68aed3
Tom Alcorn <s2517781@ed.ac.uk>
2025-03-08 20:26:55 +0000

    sketched out necessary functions

--- static/scripts/diplomats.js
+++ static/scripts/diplomats.js
@@ -1,3 +1,16 @@
 let sectors = {};
 
+// When rebel diplomat button clicked
+document.getElementById('rebel-diplomats').addEventListener('click', => {
 
+})
+
+// When empire diplomat button clicked
+document.getElementById('rebel-diplomats').addEventListener('click', => {
+    
+})
+
+// When clear diplomats button clicked
+document.getElementById('clear-diplomats').addEventListener('click', => {
+    
+})
